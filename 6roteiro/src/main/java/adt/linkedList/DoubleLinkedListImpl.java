5bf5f21737d92fcc6544d80e75f032f757177cd8
joaobb <joao.pedro.barbosa@ccc.ufcg.edu.br>
2018-10-02 23:41:31 -0300

    Melhorias no roteiro 6

--- 6roteiro/src/main/java/adt/linkedList/DoubleLinkedListImpl.java
+++ 6roteiro/src/main/java/adt/linkedList/DoubleLinkedListImpl.java
@@ -5,47 +5,61 @@ public class DoubleLinkedListImpl<T> extends SingleLinkedListImpl<T> implements
 
     protected DoubleLinkedListNode<T> last;
 
+    public DoubleLinkedListImpl() {
+        this.last = new DoubleLinkedListNode<>();
+        this.head = last;
+    }
+
+    @Override
+    public void insert(T element) {
+        if (element != null) {
+
+            DoubleLinkedListNode<T> node = new DoubleLinkedListNode<>(element, new DoubleLinkedListNode<>(), this.last);
+            this.last = node;
+
+
+        }
+    }
+
     @Override
     public void insertFirst(T element) {
-        if (element != null && this.head instanceof DoubleLinkedList) {
-            DoubleLinkedListNode<T> currentHead = (DoubleLinkedListNode<T>) this.getHead();
-            DoubleLinkedListNode<T> newHead = new DoubleLinkedListNode<>();
-            newHead.setNext(currentHead);
-            newHead.setPrevious(new DoubleLinkedListNode<>());
-            newHead.setData(element);
-            currentHead.setPrevious(newHead);
-            this.setHead(newHead);
+        if (element != null) {
+            DoubleLinkedListNode<T> newHead = new DoubleLinkedListNode<>(element, null, new DoubleLinkedListNode<>());
+            newHead.setNext(head);
 
-            if (this.head.isNIL()) {
+            if (head instanceof DoubleLinkedListNode)
+                ((DoubleLinkedListNode<T>) head).setPrevious(newHead);
+
+            if (head.isNIL())
                 this.last = newHead;
-            }
-            head = newHead;
+
+            this.setHead(newHead);
         }
     }
 
     @Override
     public void removeFirst() {
-        if (this.getHead() instanceof DoubleLinkedList) {
-            DoubleLinkedListNode<T> currentHead = (DoubleLinkedListNode<T>) this.getHead();
+        if (!this.head.isNIL())
+            this.setHead(this.head.next);
+        else
+            this.last = (DoubleLinkedListNode<T>) this.head;
 
-            if (!currentHead.isNIL()) {
-                currentHead = (DoubleLinkedListNode<T>) currentHead.getNext();
-                if (currentHead.isNIL()) {
-                    last = currentHead;
-                }
-                currentHead.setPrevious(new DoubleLinkedListNode<>());
-            }
-        }
+        if (this.getHead() instanceof DoubleLinkedList)
+            ((DoubleLinkedListNode<T>) this.head).setPrevious(new DoubleLinkedListNode<>());
     }
+
 
     @Override
     public void removeLast() {
+        if (this.last == null) this.last = new DoubleLinkedListNode<>();
+
         if (!this.last.isNIL()) {
-            this.last = this.last.getPrevious();
-            if (this.last.isNIL()) {
-                this.head = last;
-            }
-            this.last.next = new DoubleLinkedListNode<>();
+            this.last = last.getPrevious();
+
+            if (this.last.isNIL())
+                head = last;
+
+            last.next = new DoubleLinkedListNode<>();
         }
     }
 
